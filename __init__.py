68971257733cfa4411de0600f97c074546b4614c
Blender Defender <contact.blenderdefender@gmail.com>
2023-08-29 12:51:44 +0200

    chore: Add GPL3 license block to `__init__.py`
    To properly apply the GPL3 license, the GPL license block has been added to the top of `__init__.py`.
    The file has been formatted with autopep8.
    `bl_info` was edited to set location and description to TODO

--- __init__.py
+++ __init__.py
@@ -1,19 +1,29 @@
-bl_info = {
-    "name": "Super Advanced Compositor",
-    "author": "Kevin Lorengel, Slinc",
-    "version": (0, 0, 1),
-    "blender": (3, 6, 0),
-    "location": "View3D > Add > Mesh > New Object",
-    "description": "Adds a new Mesh Object",
-    "warning": "",
-    "doc_url": "",
-    "category": "Compositor",
-}
+# ##### BEGIN GPL LICENSE BLOCK #####
+#
+#  <one line to give the program's name and a brief idea of what it does.>
+#    Copyright (C) <2023>  <Kevin Lorengel>
+#
+#  This program is free software; you can redistribute it and/or
+#  modify it under the terms of the GNU General Public License
+#  as published by the Free Software Foundation; either version 3
+#  of the License, or (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software Foundation,
+#  Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+#  Alternatively, see <https://www.gnu.org/licenses/>.
+#
+# ##### END GPL LICENSE BLOCK #####
 
 import bpy
 
-from .SAC_Settings import (
-    SAC_Settings
+from .SAC_Operators import (
+    SAC_OT_Initialize
 )
 
 from .SAC_Panel import (
@@ -27,9 +37,22 @@ from .SAC_Panel import (
     SAC_PT_EFFECTS_Panel
 )
 
-from .SAC_Operators import (
-    SAC_OT_Initialize
+from .SAC_Settings import (
+    SAC_Settings
 )
+
+bl_info = {
+    "name": "Super Advanced Compositor",
+    "author": "Kevin Lorengel, Slinc",
+    "version": (0, 0, 1),
+    "blender": (3, 6, 0),
+    "location": "TODO",
+    "description": "TODO",
+    "warning": "",
+    "doc_url": "",
+    "category": "Compositor",
+}
+
 
 classes = (
     SAC_PT_SAC_Panel,
@@ -44,13 +67,15 @@ classes = (
     SAC_OT_Initialize,
 
     SAC_Settings
-    )
+)
+
 
 def register():
     # register the example panel, to show updater buttons
     for cls in classes:
         bpy.utils.register_class(cls)
     bpy.types.Scene.sac_settings = bpy.props.PointerProperty(type=SAC_Settings)
+
 
 def unregister():
     # register the example panel, to show updater buttons
